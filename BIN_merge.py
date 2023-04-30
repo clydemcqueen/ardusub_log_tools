ce8c0c8df8240713ff6d5678d3ad6b9c4fa1ce03
Clyde McQueen <clyde@mcqueen.net>
2023-04-30 14:23:45 -0700

    Create a BIN_merge tool

--- BIN_merge.py
+++ BIN_merge.py
@@ -1,54 +1,161 @@
 #!/usr/bin/env python3
 
 """
-Read MAVLink messages from a tlog file (telemetry log) and merge the messages into a single, wide csv file. The merge
+Read ArduSub dataflash messages from a BIN file and merge the messages into a single, wide csv file. The merge
 operation does a forward-fill (data is copied from the previous row), so the resulting merged csv file may be
 substantially larger than the sum of the per-type csv files.
 """
 
 from argparse import ArgumentParser
-import os
 import pandas as pd
 from pymavlink import mavutil
-import table_types
 import util
 
-# Use MAVLink2
-os.environ['MAVLINK20'] = '1'
+# Basically everything I've seen in an ArduSub dataflash (BIN) file
+ALL_MSG_TYPES = [
+    'AHR2',
+    'ARM',
+    'ATT',
+    'BARO',
+    'BAT',
+    'CTRL',
+    'CTUN',
+    'DSF',
+    'DU32',
+    'EV',
+    'FMT',
+    'FMTU',
+    'FTN',
+    'IMU',
+    'MAG',
+    'MAV',
+    'MAVC',
+    'MODE',
+    'MOTB',
+    'MSG',
+    'MULT',
+    'PARM',
+    'PM',
+    'PSCD',
+    'RATE',
+    'RCI2',
+    'RCIN',
+    'RCOU',
+    'UNIT',
+    'VIBE',
+    'XKF1',
+    'XKF2',
+    'XKF3',
+    'XKF4',
+    'XKF5',
+    'XKFS',
+    'XKQ',
+    'XKT',
+    'XKV1',
+    'XKV2',
+]
+
+# Stuff that looks kinda interesting
+PERHAPS_USEFUL_MSG_TYPES = [
+    'AHR2',
+    'ARM',
+    'ATT',
+    'BARO',
+    'BAT',
+    'CTRL',
+    'CTUN',
+    'DSF',
+    'DU32',
+    'EV',
+    'FMT',
+    'FMTU',
+    'FTN',
+    'IMU',
+    'MAG',
+    'MAV',
+    'MAVC',
+    'MODE',
+    'MOTB',
+    'MSG',
+    'MULT',
+    'PARM',
+    'PM',
+    'PSCD',
+    'RATE',
+    'RCI2',
+    'RCIN',
+    'RCOU',
+]
+
+# Useful for surftrak testing, this is the default for now
+SURFTRAK_MSG_TYPES = [
+    'ARM',
+    'ATT',
+    'BAT',
+    'CTUN',
+    'MODE',
+    'RCIN',
+    'RCOU',
+]
 
 
-class TelemetryLogReader:
+class DataflashTable:
+    @staticmethod
+    def create_table(msg_type: str):
+        return DataflashTable(msg_type)
+
+    def __init__(self, msg_type: str):
+        self._msg_type = msg_type
+        self._rows = []
+        self._df = None
+
+    def append(self, row: dict):
+        self._rows.append(row)
+
+    def get_dataframe(self, verbose):
+        if self._df is None:
+            self._df = pd.DataFrame(self._rows)
+            if verbose:
+                print('-----------------')
+                if self._df.empty:
+                    print(f'{self._msg_type} is empty')
+                else:
+                    print(f'{self._msg_type} has {len(self._df)} rows:')
+                    print(self._df.head())
+
+        return self._df
+
+
+class DataflashLogReader:
     def __init__(self,
-                 tlog_filename: str,
+                 input_filename: str,
                  msg_types: list[str],
                  max_msgs: int,
                  max_rows: int,
                  verbose: bool):
-        self.tlog_filename = tlog_filename
-        self.prefix = tlog_filename.split('.')[0]
+        self.input_filename = input_filename
+        self.prefix = input_filename.split('.')[0]
         self.msg_types = msg_types
         self.max_msgs = max_msgs
         self.max_rows = max_rows
         self.verbose = verbose
         self.tables = None
 
-    def read_tlog(self):
+    def read(self):
         self.tables = {}
         for msg_type in self.msg_types:
-            self.tables[msg_type] = table_types.Table.create_table(msg_type)
+            self.tables[msg_type] = DataflashTable.create_table(msg_type)
 
-        print(f'Reading {self.tlog_filename}')
-        mlog = mavutil.mavlink_connection(self.tlog_filename, robust_parsing=False, dialect='ardupilotmega')
+        print(f'Reading {self.input_filename}')
+        mlog = mavutil.mavlink_connection(self.input_filename, robust_parsing=False, dialect='ardupilotmega')
 
         print('Parsing messages')
         msg_count = 0
         while (msg := mlog.recv_match(blocking=False, type=self.msg_types)) is not None:
-            # Only consider messages from ArduSub
-            if msg.get_srcSystem() != 1 or msg.get_srcComponent() != 1:
-                continue
-
             msg_type = msg.get_type()
             raw_data = msg.to_dict()
+
+            # This will pull from TimeUS, which is present in all (most?) records
             timestamp = getattr(msg, '_timestamp', 0.0)
 
             # Clean up the data: add the timestamp, remove mavpackettype, and rename the keys
@@ -105,13 +212,10 @@ class TelemetryLogReader:
 
 def main():
     # TODO the prefix method isn't working now
-    # TODO param time granularity
-    # TODO TIMESYNC?
-    # TODO SYSTEM_TIME?
 
     parser = ArgumentParser(description=__doc__)
     parser.add_argument('-r', '--recurse', action='store_true',
-                        help='enter directories looking for tlog files')
+                        help='enter directories looking for BIN files')
     parser.add_argument('-v', '--verbose', action='store_true',
                         help='print a lot more information')
     parser.add_argument('--explode', action='store_true',
@@ -126,68 +230,22 @@ def main():
                         help='stop if the merged table exceeds this number of rows (default 500K)')
     parser.add_argument('paths', nargs='+')
     args = parser.parse_args()
-    files = util.expand_path(args.paths, args.recurse, '.tlog')
+    files = util.expand_path(args.paths, args.recurse, '.BIN')
     print(f'Processing {len(files)} files')
 
     if args.types:
         msg_types = args.types.split(',')
     else:
-        # Basically everything I've seen in an ArduSub tlog file
-        msg_types = [
-            'AHRS',
-            'AHRS2',
-            'ATTITUDE',
-            # 'AUTOPILOT_VERSION',
-            'BATTERY_STATUS',
-            # 'COMMAND_ACK',
-            # 'COMMAND_LONG',
-            # 'DISTANCE_SENSOR',
-            'EKF_STATUS_REPORT',
-            'GLOBAL_POSITION_INT',
-            'GPS2_RAW',
-            'GPS_GLOBAL_ORIGIN',
-            'GPS_RAW_INT',
-            'HEARTBEAT',
-            # 'HOME_POSITION',
-            'HWSTATUS',
-            'LOCAL_POSITION_NED',
-            # 'MANUAL_CONTROL',
-            'MEMINFO',
-            # 'MISSION_ACK',
-            # 'MISSION_COUNT',
-            'MISSION_CURRENT',
-            # 'MISSION_REQUEST_LIST',
-            'MOUNT_STATUS',
-            'NAMED_VALUE_FLOAT',
-            'NAV_CONTROLLER_OUTPUT',
-            # 'PARAM_REQUEST_LIST',
-            # 'PARAM_VALUE',
-            'POWER_STATUS',
-            'RANGEFINDER',
-            'RAW_IMU',
-            'RC_CHANNELS',
-            # 'REQUEST_DATA_STREAM',
-            'SCALED_IMU2',
-            'SCALED_PRESSURE',
-            'SCALED_PRESSURE2',
-            'SENSOR_OFFSETS',
-            'SERVO_OUTPUT_RAW',
-            # 'STATUSTEXT',
-            'SYS_STATUS',
-            'SYSTEM_TIME',
-            'TIMESYNC',
-            'VFR_HUD',
-            'VIBRATION',
-        ]
+        msg_types = SURFTRAK_MSG_TYPES
 
     for file in files:
         print('===================')
-        tlog_reader = TelemetryLogReader(file, msg_types, args.max_msgs, args.max_rows, args.verbose)
-        tlog_reader.read_tlog()
+        reader = DataflashLogReader(file, msg_types, args.max_msgs, args.max_rows, args.verbose)
+        reader.read()
         if args.explode:
-            tlog_reader.write_msg_csv_files()
+            reader.write_msg_csv_files()
         if not args.no_merge:
-            tlog_reader.write_merged_csv_file()
+            reader.write_merged_csv_file()
 
 
 if __name__ == '__main__':
--- util.py
+++ util.py
@@ -2,13 +2,13 @@ import glob
 import os
 
 
-def expand_path(paths: list[str], recurse: bool) -> set[str]:
+def expand_path(paths: list[str], recurse: bool, ext: str) -> set[str]:
     files = set()
 
     for path in paths:
         if os.path.isfile(path):
             file_name, file_ext = os.path.splitext(path)
-            if file_ext == '.tlog':
+            if file_ext == ext:
                 files.add(path)
         else:
             if recurse:
