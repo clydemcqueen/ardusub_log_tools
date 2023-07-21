5ed488efd24fc54708a6d383b2ccaaed6e559c56
Clyde McQueen <clyde@mcqueen.net>
2023-07-21 09:01:54 -0700

    Look for GPS datetime information

--- BIN_info.py
+++ BIN_info.py
@@ -22,15 +22,34 @@ class DataflashLogInfo:
         print(f'Results for {self.infile}')
         mlog = mavutil.mavlink_connection(self.infile, robust_parsing=False, dialect='ardupilotmega')
 
-        while (msg := mlog.recv_match(blocking=False, type=['MSG'])) is not None:
+        count_gps_records = 0
+        gps_week = 0
+        gps_week_ms = 0
+
+        while (msg := mlog.recv_match(blocking=False, type=['MSG', 'GPS'])) is not None:
             raw_data = msg.to_dict()
+            msg_type = raw_data['mavpackettype']
 
-            message = raw_data['Message']
-            if message not in self.message_counts:
-                self.message_counts[message] = 0
-            self.message_counts[message] += 1
+            if msg_type == 'MSG':
+                message = raw_data['Message']
+                if message not in self.message_counts:
+                    self.message_counts[message] = 0
+                self.message_counts[message] += 1
 
-        # Print results
+            elif msg_type == 'GPS' and gps_week == 0:
+                count_gps_records += 1
+                if raw_data['GWk'] > 0:
+                    gps_week = raw_data['GWk']
+                    gps_week_ms = raw_data['GMS']
+
+        if count_gps_records == 0:
+            print('No GPS records, no datetime information')
+        elif gps_week == 0:
+            print(f'{count_gps_records} GPS records, gps_week is always 0, no datetime information')
+        else:
+            print(f'{count_gps_records} GPS records, last record gps_week {gps_week}, gps_week_ms {gps_week_ms}')
+
+        print('List of messages, with counts:')
         for item in sorted(self.message_counts.items()):
             print(f'{item[1]:8d}  {item[0]}')
 
