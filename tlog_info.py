c4f3dc21b1ef9f068b9c3a0f1792b1dfcaa12033
Clyde McQueen <clyde@mcqueen.net>
2023-09-15 11:15:40 -0700

    Report on sensor health

--- tlog_info.py
+++ tlog_info.py
@@ -12,6 +12,35 @@ from pymavlink import mavutil
 import util
 
 
+class SensorInfo:
+    def __init__(self, description: str):
+        if description == '0x100 laser based position':
+            description = '0x100 laser based position (down-facing sonar rangefinder)'
+
+        self.description = description
+        self.present = 0
+        self.enabled = 0
+        self.healthy = 0
+
+    def count(self, enabled, healthy):
+        self.present += 1
+        if enabled:
+            self.enabled += 1
+        if healthy:
+            self.healthy += 1
+
+    def count_str(self, count: int) -> str:
+        if count == 0:
+            return 'NEVER!'
+        elif count == self.present:
+            return 'always'
+        else:
+            return f'{100.0 * count / self.present :.2f}%'
+
+    def report(self) -> str:
+        return f'{self.description}: enabled {self.count_str(self.enabled)}, healthy {self.count_str(self.healthy)}'
+
+
 class CompInfo:
     @staticmethod
     def sys_name(sys_id: int) -> str:
@@ -66,7 +95,10 @@ class CompInfo:
         # Count # of non-zero SYSTEM_TIME.time_unix_usec values
         self._unix_time = 0
 
-    def scan(self, msg):
+        # Keep stats on sensor health
+        self._sensors_present: dict[int, SensorInfo] | None = None
+
+    def process_msg(self, msg):
         msg_type = msg.get_type()
         data = msg.to_dict()
 
@@ -93,6 +125,22 @@ class CompInfo:
 
             if data['time_unix_usec'] != 0:
                 self._unix_time += 1
+
+        elif msg_type == 'SYS_STATUS':
+
+            if self._sensors_present is None:
+                self._sensors_present = {}
+
+            for bit, entry in apm.enums['MAV_SYS_STATUS_SENSOR'].items():
+                if bit == apm.MAV_SYS_STATUS_SENSOR_ENUM_END:
+                    break
+                if msg.onboard_control_sensors_present & bit:
+                    if bit not in self._sensors_present:
+                        self._sensors_present[bit] = SensorInfo(entry.description)
+
+                    enabled = msg.onboard_control_sensors_enabled & bit
+                    healthy = msg.onboard_control_sensors_health & bit
+                    self._sensors_present[bit].count(enabled, healthy)
 
     def report_heartbeat(self):
         print('            HEARTBEAT')
@@ -134,10 +182,18 @@ class CompInfo:
             print('            SYSTEM_TIME')
             print(f'                         valid time was sent {self._unix_time} time(s)')
 
+    def report_sys_status(self):
+        if self._sensors_present is not None:
+            print('            SYS_STATUS')
+            print('                sensors')
+            for _, info in self._sensors_present.items():
+                print('                         ' + info.report())
+
     def report(self):
         self.report_heartbeat()
         self.report_statustext()
         self.report_system_time()
+        self.report_sys_status()
 
 
 class TelemetryLogInfo:
@@ -151,7 +207,8 @@ class TelemetryLogInfo:
         # Build a dictionary sys_id => system
         # Each system is a dictionary of comp_id => instance of CompInfo
         systems = {}
-        while (msg := mlog.recv_match(blocking=False, type=['HEARTBEAT', 'STATUSTEXT', 'SYSTEM_TIME'])) is not None:
+        while (msg := mlog.recv_match(blocking=False,
+                                      type=['HEARTBEAT', 'STATUSTEXT', 'SYSTEM_TIME', 'SYS_STATUS'])) is not None:
             sys_id = msg.get_srcSystem()
 
             if sys_id not in systems:
@@ -163,7 +220,7 @@ class TelemetryLogInfo:
                 systems[sys_id][comp_id] = CompInfo(sys_id, comp_id)
 
             # Scan records for interesting info
-            systems[sys_id][comp_id].scan(msg)
+            systems[sys_id][comp_id].process_msg(msg)
 
         # Print results
         for si in sorted(systems.items()):
@@ -175,7 +232,8 @@ class TelemetryLogInfo:
 
 def main():
     parser = ArgumentParser(description=__doc__)
-    parser.add_argument('-r', '--recurse', help='enter directories looking for tlog files', action='store_true')
+    parser.add_argument('-r', '--recurse',
+                        help='enter directories looking for tlog files', action='store_true')
     parser.add_argument('path', nargs='+')
     args = parser.parse_args()
     files = util.expand_path(args.path, args.recurse, '.tlog')
