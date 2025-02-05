04de75658cdfd10ff08b7913ac8a0eb7cedce32b
Clyde McQueen <clyde@mcqueen.net>
2025-02-05 07:46:03 -0800

    Add check for future time in tlog_bad_time

--- tlog_bad_time.py
+++ tlog_bad_time.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 
 """
-Read MAVLink messages from a tlog file (telemetry log) and check to see if time goes backwards.
+Read a tlog file (telemetry log) and report on bad timestamps.
 
 Supports segments.
 """
 
+import time
 from argparse import ArgumentParser
 
 from segment_reader import add_segment_args, choose_reader_list
@@ -14,15 +15,19 @@ from segment_reader import add_segment_args, choose_reader_list
 def check_timestamps(reader):
     count = 0
     prev_timestamp = None
+    tomorrow = time.time() + 24 * 60 * 60
 
     for msg in reader:
         sysid = msg.get_srcSystem()
         compid = msg.get_srcComponent()
+        msg_type = msg.get_type()
         timestamp = getattr(msg, '_timestamp', 0.0)
 
-        if prev_timestamp is not None:
-            if timestamp < prev_timestamp:
-                print(f'Time goes backwards: count={count}, sysid={sysid}, compid={compid}, {prev_timestamp} to {timestamp}')
+        if timestamp > tomorrow:
+            print(f'  Time {timestamp} is in the future; message num={count}, type={msg_type}, sysid={sysid}, compid={compid}')
+
+        if prev_timestamp is not None and timestamp < prev_timestamp:
+            print(f'  Time went backwards from {prev_timestamp} to {timestamp}; message num={count}, sysid={sysid}, compid={compid}')
 
         count += 1
         prev_timestamp = timestamp
@@ -36,6 +41,7 @@ def main():
     msg_types = None if args.types is None else args.types.split(',')
     readers = choose_reader_list(args, msg_types)
     for reader in readers:
+        print(f'Scanning {reader.name}...')
         check_timestamps(reader)
 
 
